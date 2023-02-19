7504baa1a69d187dae3247157953db7e44312ce8
WKJYES <iwkj@foxmail.com>
2023-02-19 22:05:01 +0800

    下班了，回去继续写

--- app/src/main/java/com/example/kexieqiandao/network/HttpManager.java
+++ app/src/main/java/com/example/kexieqiandao/network/HttpManager.java
@@ -1,8 +1,20 @@
 package com.example.kexieqiandao.network;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.kexieqiandao.MyApplication;
+import com.example.kexieqiandao.entity.UserId;
 import com.example.kexieqiandao.network.retrofit_api.IService;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -27,5 +39,23 @@ public class HttpManager {
         service = retrofit.create(IService.class);
     }
 
-    public void qiandao()
+    public void qiandao(MutableLiveData<String> uid,MutableLiveData<String> uname,MutableLiveData<String> time,MutableLiveData<String> totalTime,MutableLiveData<Boolean> online,MutableLiveData<String> qiandaoStatus){
+        UserId userId=new UserId();
+        userId.setUserId(uid.getValue());
+        service.signIn(userId).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                } catch (JSONException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                qiandaoStatus.setValue(MyApplication.qiandaoFail);
+            }
+        });
+    }
 }
